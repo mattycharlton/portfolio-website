129a1381be28414fa3c801d0e3967f1154629dec
Matthew <mattycharlton101@gmail.com>
2020-04-18 01:07:39 +0100

    create footer component base

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React, { Component, Fragment } from 'react';
+import styles from './footer.scss';
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        <p>mattycharlton.co.uk - 2020</p>
+    </footer>
+);
+
+export default Footer;
