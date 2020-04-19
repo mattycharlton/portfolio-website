61e0cbe293422e44c954d805f280bd4271b8ff40
Matthew <mattycharlton101@gmail.com>
2020-04-19 01:17:43 +0100

    add header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { A } from 'hookrouter';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import styles from './header.scss';
+import Nav from '../nav/nav';
+import Logo from '../../images/logo.png';
 
-const Header = () => (
-  <nav className={styles.header}>
-  <ul>
-      <li><A href="/">Home</A></li>
-      <li><A href="/about">About</A></li>
-      <li><A href="/contact">Contact</A></li>
-      <li><A href="/skills">Skills</A></li>
-  </ul>
-  </nav>
-);
+
+const Header = () => {
+  const [navValue, setNavValue] = useState(false);
+  return <header className={styles.header}>
+    <Nav isNavOpen={navValue} onNavChange={setNavValue} />
+  </header>
+};
 
 export default Header;
