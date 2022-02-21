6789972e86194fceb02cf703622e3c4a69dc288d
Matthew Charlton <mattycharlton101@gmail.com>
2022-02-21 00:07:35 +0000

    Adds strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 import App from './App';
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, mountNode);
